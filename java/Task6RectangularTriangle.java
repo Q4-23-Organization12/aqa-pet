2caf1c5ff05a32f4a08160fb20b95305add9c473
Grynko Andrey <115623463+benteylor@users.noreply.github.com>
2023-12-21 17:49:58 +0200

    Merge remote-tracking branch 'origin/main'

--- java/Task6RectangularTriangle.java
+++ java/Task6RectangularTriangle.java
@@ -6,6 +6,6 @@ public class Task6RectangularTriangle {
         // Расчёт
         double S = sideA * sideB/2;
         // Результат
-        System.out.printf("Kershaw: "  +S);
+        System.out.printf("1111: "  +S);
     }
 }
