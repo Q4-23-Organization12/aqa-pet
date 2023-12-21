c5c168f3245442b1cfc5acf5d64721a4da021740
Grynko Andrey <115623463+benteylor@users.noreply.github.com>
2023-12-21 11:35:29 +0200

    push homework number 5_6

--- java/Task4DataTypesOperators.java
+++ java/Task4DataTypesOperators.java
@@ -1,5 +1,5 @@
 public class Task4DataTypesOperators {
     public static void main(String[] args) {
-        System.out.print("1234");
+        System.out.print("12334");
     }
 }
--- java/Task6RectangularTriangle.java
+++ java/Task6RectangularTriangle.java
@@ -1,7 +1,7 @@
 public class Task6RectangularTriangle {
     public static void main(String[] args) {
         // Стороны треугольника
-        double sideA = 11;
+        double sideA = 2;
         double sideB = 11;
         // Расчёт
         double S = sideA * sideB/2;
